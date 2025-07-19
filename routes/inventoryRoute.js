9bd075473e5f0ab298c6a03dc838e361e9f2a37f
antchristensen <anthony.christensen1988@gmail.com>
2025-07-18 19:34:12 -0600

    inventory error handling

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,11 +2,18 @@
 const express = require("express")
 const router = new express.Router()
 const invController = require("../controllers/invController")
+const utilities = require("../utilities/")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId)
+router.get(
+  "/type/:classificationId",
+  utilities.handleErrors(invController.buildByClassificationId)
+)
 
 // Route to build inventory detail view by inventory ID
-router.get("/detail/:invId", invController.buildDetail)
+router.get(
+  "/detail/:invId",
+  utilities.handleErrors(invController.buildDetail)
+)
 
 module.exports = router
